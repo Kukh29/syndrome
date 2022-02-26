0e7ea1e912155af07afdb803f3a25c59ef8af608
kukh29 <kukh29@gmail.com>
2022-02-26 13:00:57 +0300

    syndrome

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -46,7 +46,7 @@ public class StatisticsService {
         long current_max = incomes[0];
         for (long income : incomes)
             if (current_max <= income)
-                current_max = 12;
+                current_max = incomes[0];
         return current_max;
     }
 }
