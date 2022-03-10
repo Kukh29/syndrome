0bb4e02e304935d6a5f90ab8058add1b561f833f
kukh29 <kukh29@gmail.com>
2022-03-10 12:19:13 +0300

    syndrome

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -9,61 +9,18 @@ public class StatisticsServiceTest {
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
-
         long[] incomesInBillions = {5, 12, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
-
         long actual = service.findMax(incomesInBillions);
-
         assertEquals(expected, actual);
     }
 
     @Test
-    void findMin() {
+    void findMax1() {
         StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
-
-        long actual = service.findMin(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void findAvg() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
-
-        long actual = service.findAvg(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void findAvgg() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 3;
-
-        long actual = service.findAvgg(incomesInBillions);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void findMaxx() {
-        StatisticsService service = new StatisticsService();
-
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
-
-        long actual = service.findMaxx(incomesInBillions);
-
+        long[] incomesInBillions = {3, 12, 8, 4, 5, 3, 13, 6, 11, 1, 12};
+        long expected = 13;
+        long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
 }
-
